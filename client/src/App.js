6766200014605fff42454cab79df47c87b734ebd
Roshan Mahesh <roshanmahesh@Roshans-MacBook-Pro.local>
2024-03-18 22:56:55 -0500

    Communication between front and backend established

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:80/');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setMessage(data.message);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from the frontend!</h1>
+      <p>Message from the backend: {message}</p>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-//
+var express = require('express');
+var bodyParser = require('body-parser');
+const cors = require('cors')
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(cors());    // need to use this since frontend and backend are running on different ports
+// may not need to use cors after building it/not running all of this locally
+
+app.get('/', function(req, res) {
+    res.send({message: 'No'});   // sending message "Hello" to the frontend
+});
+
+app.listen(80, function() {
+    console.log('Node app is running on port 80');
+});
